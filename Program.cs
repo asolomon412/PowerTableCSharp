1d56f4aeff5353f1c5bcf458d80b557ef0e4f1f2
asolomon412 <antonellasolomon@gmail.com>
2019-01-15 13:37:06 -0500

    fixed some formatting

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ namespace Lab4
         private static void PowerGenerator(int num)
         {
             Console.WriteLine("Number\tSquared\t Cubed");
-            Console.WriteLine("======\t=======\t=====");
+            Console.WriteLine("======\t=======\t =====");
             for (int i = 1; i <= num; i++)
             {
                 Console.WriteLine(i + "\t" + (int)Math.Pow(i, 2) + "\t" + (int)Math.Pow(i, 3));
