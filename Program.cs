cf1eabc16bd337f12396e9345388df14fbbc3ab0
asolomon412 <antonellasolomon@gmail.com>
2019-01-15 11:49:23 -0500

    refactored, extracted method

--- Program.cs
+++ Program.cs
@@ -15,12 +15,7 @@ namespace Lab4
                 Console.WriteLine("Please enter an integer: ");
                 int num = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("Number\tSquared\t Cubed");
-                Console.WriteLine("======\t=======\t=====");
-                for (int i = 1; i <= num; i++)
-                {
-                    Console.WriteLine(i+"\t" + (int)Math.Pow(i,2) +"\t"+ (int)Math.Pow(i, 3));
-                }
+                PowerGenerator(num);
 
                 Console.WriteLine("Continue? y/n ");
                 string keepGoing = Console.ReadLine();
@@ -29,7 +24,17 @@ namespace Lab4
                     Console.WriteLine("Goodbye!");
                     break;
                 }
-                
+
+            }
+        }
+
+        private static void PowerGenerator(int num)
+        {
+            Console.WriteLine("Number\tSquared\t Cubed");
+            Console.WriteLine("======\t=======\t=====");
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine(i + "\t" + (int)Math.Pow(i, 2) + "\t" + (int)Math.Pow(i, 3));
             }
         }
     }
