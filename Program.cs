6b58c6958b8ebb2e71f604d4664c770fe6960dd0
asolomon412 <antonellasolomon@gmail.com>
2019-01-15 11:40:53 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter an integer: ");
+                int num = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Number\tSquared\t Cubed");
+                Console.WriteLine("======\t=======\t=====");
+                for (int i = 1; i <= num; i++)
+                {
+                    Console.WriteLine(i+"\t" + (int)Math.Pow(i,2) +"\t"+ (int)Math.Pow(i, 3));
+                }
+
+                Console.WriteLine("Continue? y/n ");
+                string keepGoing = Console.ReadLine();
+                if (keepGoing.ToLower().Equals("n"))
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+                
+            }
+        }
+    }
+}
